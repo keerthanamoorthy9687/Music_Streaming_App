880bd03fd0d5ee28acab4a36a310065157f7b3fc
keerthanamoorthy9687 <dheeshnamoorthy9687@gmail.com>
2025-04-07 12:54:50 +0530

    Adding the player function ang display a songs

---
+++ client/src/assets/assets.js
@@ -0,0 +1,143 @@
+import bell_icon from './bell.png'
+import home_icon from './home.png'
+import like_icon from './like.png'
+import loop_icon from './loop.png'
+import mic_icon from './mic.png'
+import next_icon from './next.png'
+import play_icon from './play.png'
+import pause_icon from './pause.png'
+import plays_icon from './plays.png'
+import prev_icon from './prev.png'
+import search_icon from './search.png'
+import shuffle_icon from './shuffle.png'
+import speaker_icon from './speaker.png'
+import stack_icon from './stack.png'
+import zoom_icon from './zoom.png'
+import plus_icon from './plus.png'
+import arrow_icon from './arrow.png'
+import mini_player_icon from './mini-player.png'
+import queue_icon from './queue.png'
+import volume_icon from './volume.png'
+import arrow_right from './right_arrow.png'
+import arrow_left from './left_arrow.png'
+import spotify_logo from './spotify_logo.png'
+import clock_icon from './clock_icon.png'
+import mute_icon from './mute_icon.png'
+import music_gif from './music_gif.gif'
+
+//image
+import image1 from './img1.jpg'
+import image2 from './img2.jpg'
+import image3 from './img3.jpg'
+import image4 from './img4.jpg'
+import image5 from './img5.jpg'
+
+// songs
+import song1 from "./song1.mp3"
+import song2 from "./song2.mp3"
+import song3 from "./song3.mp3"
+
+
+export const assets = {
+    bell_icon,
+    home_icon,
+    like_icon,
+    loop_icon,
+    mic_icon,
+    next_icon,
+    play_icon,
+    plays_icon,
+    prev_icon,
+    search_icon,
+    shuffle_icon,
+    speaker_icon,
+    stack_icon,
+    zoom_icon,
+    plus_icon,
+    arrow_icon,
+    mini_player_icon,
+    volume_icon,
+    queue_icon,
+    pause_icon,
+    arrow_left,
+    arrow_right,
+    spotify_logo,
+    clock_icon,
+    mute_icon,
+    music_gif
+}
+
+
+  export const albumData=[
+    {
+        id:0,
+        name:"Top 5 Songs",
+        image:image1,
+        desc:"your daily update on most played tracks",
+        bgColor:"#44337a"
+    },
+     {
+        id:1,
+        name:"Top 50 Songs",
+        image:image2,
+        desc:"your weekly update on most played tracks",
+        bgColor:"#44337a"
+    },
+     {
+        id:2,
+        name:"Midnight Vibes",
+        image:image3,
+        desc:"your daily update on most played tracks",
+        bgColor:"#44337a"
+    },
+     {
+        id:3,
+        name:"Top 5 Songs",
+        image:image4,
+        desc:"your daily update on most played tracks",
+        bgColor:"#234e52"
+    }
+
+
+  ]
+
+  export const SongsData=[
+    {
+        id:0,
+        name:"song1",
+        image:image1,
+        file:song1,
+        desc:"put smile on your face with these happy tunes ",
+        duration:"3:00"
+    },
+     {
+        id:1,
+        name:"song2",
+        image:image2,
+        file:song2,
+        desc:"put smile on your face with these happy tunes ",
+        duration:"3:00"
+    },
+     {
+        id:3,
+        name:"song3",
+        image:image3,
+        file:song3,
+        desc:"put smile on your face with these happy tunes ",
+        duration:"3:00"
+    }, {
+        id:4,
+        name:"song4",
+        image:image4,
+        file:song1,
+        desc:"put smile on your face with these happy tunes ",
+        duration:"3:00"
+    }, {
+        id:5,
+        name:"song5",
+        image:image5,
+        file:song3,
+        desc:"put smile on your face with these happy tunes ",
+        duration:"3:00"
+    },
+  ]
